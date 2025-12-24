f2feca964d6e897b5626f95acf0937fd0a0d2c96
Nareerat-65 <nareeerat28012547@gmail.com>
2025-12-24 08:38:06 +0700

    แยก css ของ head

--- partials/admin/head_admin.php
+++ partials/admin/head_admin.php
@@ -6,34 +6,7 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@4.0.0-rc3/dist/css/adminlte.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 <link href="https://cdn.jsdelivr.net/gh/lazywasabi/thai-web-fonts@7/fonts/BaiJamjuree/BaiJamjuree.css" rel="stylesheet" />
-
-<style>
-    body {
-        background: #fbf6f4;
-        font-family: 'Bai Jamjuree', sans-serif;
-    }
-
-    .app-header {
-        background-color: #F57B39 !important;
-        color: #fff !important;
-        font-size: 20px;
-    }
-
-    .navbar {
-        position: sticky;
-        top: 0;
-        z-index: 1020;
-    }
-
-    .nav-link.active i {
-        color: #fff !important;
-    }
-
-
-    .main-header .nav-link {
-        color: #fff !important;
-    }
-</style>
+<link rel="stylesheet" href="/assets/css/admin/admin_global.css">
 
 <!-- เพิ่มเฉพาะหน้า (เช่น FullCalendar CSS ฯลฯ) -->
 <?= $extraHead ?? '' ?>
